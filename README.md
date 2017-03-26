893333db015d991d7a8e623402477a896555c792
MengGuo <guomeng3@gmail.com>
2017-03-26 20:42:08 +0200

    rm obj in states

--- README.md
+++ README.md
@@ -1,19 +1,46 @@
-- Initialize robot model in robot_model_def.py
-- Start robot node via 
+# Motion and Task Planning for Omni-Directional Ground Vehicles
+
+## Single vehicle test
+The robot is assigned a task to __ **infinitely often** load object *a* (either at region `la1` with orientation `west` or at region `la2` with orientation `east`), then unload it (either at region `ua1` with orientation `south` or at region `ua2` with orientation `north`) and visit its base `h1`, and which is specified as the LTL formula:
+```python
+one_la = '(la1 && w) || (la2 && e)'
+one_ua = '(ua1 && s)|| (ua2 && n)'
+Y1_task = '[] <> ((%s && loada) && <> (%s && unloada)) && [] <> (h1 && e)' %(one_la, one_ua)
 ```
+
+- Initialize robot model in robot_model_def.py
+ 
+- Start robot node via 
+```python
 python omni_planner.py Y1
 ```
+
 - Manually send confirmation for robot next activity:
 ```
 rostopic pub -1 activity_done_Y1 omni_fault/confirmation -- '0' 'goto' '1'
 rostopic pub -1 activity_done_Y1 omni_fault/confirmation -- '1' 'y1act' '1'
 ```
-- Manually send operation mode update to robot via
+
+- Manually send operation mode update (*faults*) to robot via
 ```
 rostopic pub -1  status_Y1 omni_fault/status -- 'type-II'
 ```
+  Then you will see that the discrete plan has been updated due to the cost update in the underlying transition system. 
 
-- Manually send request to other robot via
+## Collaboration between multiple vehicles
+
+There are four robots in
+
+- Start all four robots via (in different terminals) 
+```python
+python omni_planner.py Y1
+python omni_planner.py Y2
+python omni_planner.py Y3
+python omni_planner.py Y4
 ```
-rostopic pub -1  collab_request omni_fault/request -- 'Y2' '<> y1r1'
+
+- Manually set robot `Y2` stops due to `type-III` fault
 ```
+rostopic pub -1  status_Y2 omni_fault/status -- 'type-III'
+```
+  Then you will see the request and reply messages exchanged among the robots to negotiate which robot will share the task for `Y2`.
--- src/robot_model_def.py
+++ src/robot_model_def.py
@@ -6,23 +6,23 @@ from ltl_tools.planner import ltl_planner
 
 from math import pi as PI
 
-def combine_wps_angle(wps_label, angle_label, obj_label):
+def combine_wps_angle(wps_label, angle_label):
     nodes_label = dict()
     symbols = set()
     for wp, wp_l in wps_label.iteritems():
         for angle, angle_l in angle_label.iteritems():
-            for obj, obj_l in obj_label.iteritems():
-                node = (wp, angle, obj)
-                node_l = wp_l.union(angle_l.union(obj_l))
-                nodes_label[node] = node_l
-                symbols.update(node_l)
+            # node = ((x,y), theta)
+            node = (wp, angle)
+            node_l = wp_l.union(angle_l)
+            nodes_label[node] = node_l
+            symbols.update(node_l)
     return nodes_label, symbols
 
 
 
 # ========================================
 # robot model description starts
-robot_model=dict()
+robot_model = dict()
 
 # angle discrete
 # # [-3.1+3.1415*k/4 for k in range(8)]
@@ -64,23 +64,22 @@ node_label = {
     (4.0, 5.0): set(['h4',]),            
 }
 
-# nobj = 'no obj'
-# obj = 'has obj'
-obj_label = {
-    0: set(['nobj',]),
-    1: set(['obj',]),
-}
-
-comb_nodes, symbols = combine_wps_angle(node_label, angle_label, obj_label)
+comb_nodes, symbols = combine_wps_angle(node_label, angle_label)
 
 f_edges1 = ((3.0,1.0), (4.0,1.0),(1.0,3.0), (1.0,4.0))
 f_edges2 = ((2.0,1.0),(5.0,1.0),(1.0,5.0),(1.0,2.0))
 f_edges3 = ((5.0,4.0), (4.0,4.0),(4.0,5.0),(5.0,5.0))
 forbid_edges = [(e1,e2) for e1 in f_edges1 for e2 in f_edges1] + [(e1,e2) for e1 in f_edges2 for e2 in f_edges2] + [(e1,e2) for e1 in f_edges3 for e2 in f_edges3]
-# -------------------- YoBot 1 model --------------------
 
-# pose = ((x,y), theta, obj)
-Y1_init_pose = ((5.0, 6.0), -PI*0.5, 0)
+
+action_label={
+    'load_a': (10, '1', set(['loada'])),
+    'unload_a': (10, '1', set(['unloada'])),
+    'load_b': (10, '1', set(['loadb'])),
+    'unload_b': (10, '1', set(['unloadb'])),}
+
+# -------------------- YoBot 1 model --------------------
+Y1_init_pose = ((5.0, 6.0), -PI*0.5)
 Y1_alpha = [1.0, 1.0] # alpha depends on the mode
 
 Y1_motion = MotionFts(comb_nodes, symbols, 'Y1-workspace')
@@ -88,20 +87,78 @@ Y1_motion.set_initial(Y1_init_pose)
 Y1_motion.add_edges(forbid_edges, Y1_alpha)
 
 ########### Y1 action ##########
-Y1_action_label={
-    'load_a': (10, '(la1 || la2) && nobj', set(['loada'])),
-    'unload_a': (10, 'obj', set(['unloada'])),
-    'load_b': (10, '(lb1 || lb2) && nobj', set(['loadb'])),
-    'unload_b': (10, 'obj', set(['unloadb'])),    
-            }
-Y1_action = ActionModel(Y1_action_label)
+Y1_action = ActionModel(action_label)
 ########### Y1 task ############
-one_la = 'la1 || la2'
-one_ua = 'ua1 || ua2'
-Y1_task = '[] <> ((%s && loada) && <> (%s && unloada)) && [] <> h1' %(one_la, one_ua)
+one_la = '(la1 && w) || (la2 && e)'
+one_ua = '(ua1 && s)|| (ua2 && n)'
+Y1_task = '[] <> ((%s && loada) && <> (%s && unloada)) && [] <> (h1 && e)' %(one_la, one_ua)
 ########### Y1 initialize ############
 Y1_mode_alpha = {'normal': [1.0, 1.0], 'type-I': [0.1, 1.0], 'type-III': [0.1, 10.0]}
 robot_model['Y1']=(Y1_motion, Y1_action, Y1_task, Y1_mode_alpha)
+
+
+# -------------------- YoBot 2 model --------------------
+Y2_init_pose = ((5.0, 4.0), -PI*0.5)
+Y2_alpha = [1.0, 1.0] # alpha depends on the mode
+
+Y2_motion = MotionFts(comb_nodes, symbols, 'Y2-workspace')
+Y2_motion.set_initial(Y2_init_pose)
+Y2_motion.add_edges(forbid_edges, Y2_alpha)
+
+########### Y2 action ##########
+Y2_action = ActionModel(action_label)
+########### Y2 task ############
+one_la = '(la1 && w) || (la2 && e)'
+one_ua = '(ua1 && s)|| (ua2 && n)'
+Y2_task = '[] <> ((%s && loada) && <> (%s && unloada)) && [] <> (h2 && e)' %(one_la, one_ua)
+########### Y2 initialize ############
+Y2_mode_alpha = {'normal': [1.0, 1.0], 'type-I': [0.1, 1.0], 'type-III': [0.1, 10.0]}
+robot_model['Y2']=(Y2_motion, Y2_action, Y2_task, Y2_mode_alpha)
+
+
+# -------------------- YoBot 3 model --------------------
+Y3_init_pose = ((5.0, 5.0), -PI*0.5)
+Y3_alpha = [1.0, 1.0] # alpha depends on the mode
+
+Y3_motion = MotionFts(comb_nodes, symbols, 'Y3-workspace')
+Y3_motion.set_initial(Y3_init_pose)
+Y3_motion.add_edges(forbid_edges, Y3_alpha)
+
+########### Y3 action ##########
+Y3_action = ActionModel(action_label)
+########### Y3 task ############
+one_lb = '(lb1 && w) || (lb2 && e)'
+one_ub = '(ub1 && s)|| (ub2 && n)'
+Y3_task = '[] <> ((%s && loadb) && <> (%s && unloadb)) && [] <> (h3 && e)' %(one_lb, one_ub)
+########### Y3 initialize ############
+Y3_mode_alpha = {'normal': [1.0, 1.0], 'type-I': [0.1, 1.0], 'type-III': [0.1, 10.0]}
+robot_model['Y3']=(Y3_motion, Y3_action, Y3_task, Y3_mode_alpha)
+
+
+# -------------------- YoBot 4 model --------------------
+Y4_init_pose = ((4.0, 5.0), -PI*0.5)
+Y4_alpha = [1.0, 1.0] # alpha depends on the mode
+
+Y4_motion = MotionFts(comb_nodes, symbols, 'Y4-workspace')
+Y4_motion.set_initial(Y4_init_pose)
+Y4_motion.add_edges(forbid_edges, Y4_alpha)
+
+########### Y4 action ##########
+Y4_action = ActionModel(action_label)
+########### Y4 task ############
+one_lb = '(lb1 && w) || (lb2 && e)'
+one_ub = '(ub1 && s)|| (ub2 && n)'
+Y4_task = '[] <> ((%s && loadb) && <> (%s && unloadb)) && [] <> (h3 && e)' %(one_lb, one_ub)
+########### Y4 initialize ############
+Y4_mode_alpha = {'normal': [1.0, 1.0], 'type-I': [0.1, 1.0], 'type-III': [0.1, 10.0]}
+robot_model['Y4']=(Y4_motion, Y4_action, Y4_task, Y4_mode_alpha)
+
+
+
+
+
+
+
 
 
 
